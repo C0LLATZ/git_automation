379d0102a5d2a8df4a7c67baadb9702bec371dff
C0LLATZ <64935394+C0LLATZ@users.noreply.github.com>
2020-05-07 17:47:48 +0200

    Added -r to remove directories. Changes in subfolder visualization.

--- project.bat
+++ project.bat
@@ -6,14 +6,19 @@ set str1=%1
 IF  x%str1:-=%==x%str1% (
     GOTO:DIR
 ) ELSE (
-    IF %1 == -list (
+    IF %1 == -l (
         GOTO:list
+    ) 
+    IF %1 == -r (
+        GOTO:remove
     ) ELSE (
+        echo %1 is not a flag.
         GOTO:end
     )
 )
 
 :DIR
+IF "%2"=="" (
 IF EXIST %ProjectsFolder%%1\ (
     cd %ProjectsFolder%%1
     for /d %%e in (*) do (
@@ -23,7 +28,7 @@ IF EXIST %ProjectsFolder%%1\ (
 ) ELSE (
     mkdir %ProjectsFolder%%1
     cd %ProjectsFolder%%1
-)
+))
 
 IF NOT "%2"=="" (
     IF EXIST %ProjectsFolder%\%1\%2\ (
@@ -45,6 +50,16 @@ GOTO:end
         call :showfolders %%d
 
     )
+GOTO:end
+
+
+:remove
+    IF "%2"==""  ( echo Need to specify platform )
+    IF "%3"==""  ( echo Need to specify project )
+    IF NOT  "%2"=="" IF NOT "%3"=="" ( 
+        rmdir %ProjectsFolder%%2\%3 
+        )      
+
 GOTO:end
 
 :showfolders
